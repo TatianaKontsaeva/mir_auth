ce0035a24e9983b489c1fa3c938d28b30a1fd286
Tatiana <tanya_13.88@mail.ru>
2023-06-19 13:54:36 +0300

    add btn to homepage on login/signup pages

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,93 +1,90 @@
 <template>
-    <q-page class="q-pa-md flex flex-center">
-      <div>
-        <h5>Вход</h5>
-        <q-form @submit="Submit()" @reset="EnterReset">
-          <q-input
-            rounded
-            v-model="email"
-            label="Введите вашу почту"
-            type="email"
-            lazy-rules
-            :rules="[required]"
-          >
-          </q-input>
-          <q-input
-            rounded
-            v-model="password"
-            label="Введите ваш пароль"
-            type="password"
-            lazy-rules
-            :rules="[required]"
-          >
-          </q-input>
-          <!-- <p class="error" v-if="error">{{ error }}</p> -->
-          <div class="q-mt-md">
-            <q-btn label="Отправить" type="submit" color="primary" />
-            <q-btn
-              label="Сбросить"
-              type="reset"
-              color="primary"
-              flat
-              class="q-ml-sm"
-            />
-          </div>
-          <p class="q-pt-md">
-            Не зарегистрированы?
-            <router-link to="/Signup">Зарегистрируйтесь</router-link>
-          </p>
-        </q-form>
-      </div>
-    </q-page>
-  </template>
-  
-  <script setup>
-  import { ref } from "vue"
-  import useValidators from "src/use/formValidation"
-  import { authentication } from "src/sdk/authentication";
+  <q-page class="q-pa-md flex-center column">
+    <div>
+      <q-btn>
+        <router-link to="/">На Главную</router-link>
+      </q-btn>
+    </div>
+    <div>
+      <h5>Вход</h5>
+      <q-form @submit="Submit()" @reset="EnterReset">
+        <q-input
+          rounded
+          v-model="email"
+          label="Введите вашу почту"
+          type="email"
+          lazy-rules
+          :rules="[required]"
+        >
+        </q-input>
+        <q-input
+          rounded
+          v-model="password"
+          label="Введите ваш пароль"
+          type="password"
+          lazy-rules
+          :rules="[required]"
+        >
+        </q-input>
+        <div class="q-mt-md">
+          <q-btn label="Отправить" type="submit" color="primary" />
+          <q-btn
+            label="Сбросить"
+            type="reset"
+            color="primary"
+            flat
+            class="q-ml-sm"
+          />
+        </div>
+        <p class="q-pt-md">
+          Не зарегистрированы?
+          <router-link to="/Signup">Зарегистрируйтесь</router-link>
+        </p>
+      </q-form>
+    </div>
+  </q-page>
+</template>
 
-  import { useRouter } from "vue-router";
+<script setup>
+import { ref } from "vue";
+import useValidators from "src/use/formValidation";
+import { authentication } from "src/sdk/authentication";
 
-  
-  const { required } = useValidators();
-  const error = ref("");
-  const router = useRouter();
+import { useRouter } from "vue-router";
 
-  const email = ref("");
-  const password = ref("");
+const { required } = useValidators();
+const error = ref("");
+const router = useRouter();
 
-  const EnterReset = () => {
-      email.value = "";
-      password.value = "";
-    };
+const email = ref("");
+const password = ref("");
 
-  const Submit = () => {
-    console.log("email", email.value);
-    console.log("password", password.value);
+const EnterReset = () => {
+  email.value = "";
+  password.value = "";
+};
 
-    let resAuth = null;
+const Submit = () => {
+  console.log("email", email.value);
+  console.log("password", password.value);
 
-    authentication({
-        email: email.value,
-        password: password.value,
-      })
-    .then ((res) => {
+  let resAuth = null;
+
+  authentication({
+    email: email.value,
+    password: password.value,
+  })
+    .then((res) => {
       resAuth = res;
       console.log(resAuth);
 
       localStorage.setItem("token", resAuth.accessToken);
-     
+
       router.push("/");
-      
     })
     .catch((err) => {
-          console.log("Ошибка: ", err);
-          error.value = "Неверный логин или пароль";
+      console.log("Ошибка: ", err);
+      error.value = "Неверный логин или пароль";
     });
-
-
-    
-  
-  }
-
-  </script>
+};
+</script>
--- src/components/SignUpPage.vue
+++ src/components/SignUpPage.vue
@@ -1,89 +1,90 @@
 <template>
-    <q-page class="q-pa-md flex-center" style="width: 60%;margin: 0 auto;">
-      <div>
-        <h5>Регистрация</h5>
-        <q-form @submit="Submit()">
-          <q-input
-            outlined
-            v-model="form.user_name"
-            label="ФИО"
-            type="text"
-            lazy-rules
-            :rules="[required]"
-          >
-          </q-input>
-          <q-input
-            outlined
-            v-model="form.password"
-            label="Введите ваш пароль"
-            type="text"
-            lazy-rules
-            :rules="[required, passwordValid]"
-          >
-          </q-input>
-          <q-input
-            outlined
-            v-model="form.confirmPassword"
-            label="Повторите ваш пароль"
-            type="text"
-            lazy-rules
-            :rules="[required, equal(form.password, form.confirmPassword)]"
-          >
-          </q-input>
-          <q-input
-            outlined
-            v-model="form.email"
-            label="Введите вашу почту"
-            type="email"
-            lazy-rules
-            :rules="[required]"
-          >
-          </q-input>
-          <q-input
-            outlined
-            v-model="form.phone"
-            label="Номер телефона"
-            type="tel"
-            mask="+7-###-### ## ##"
-            lazy-rules
-           
-          >
-          </q-input>
-      
-          <div class="q-mt-md">
-            <q-btn label="Отправить" type="submit" color="primary" />
-            
-          </div>
-          <p class="q-pt-md">
-            Уже есть аккаунт? <router-link to="/Login">Войдите</router-link>
-          </p>
-        </q-form>
-      </div>
-    </q-page>
-  </template>
-  
-  <script setup>
-  import { ref } from "vue"
-  import useValidators from "src/use/formValidation"
-  import { registration } from "src/sdk/registration"
-  import { useRouter } from "vue-router";
- 
-  const { required } = useValidators();
-  const { equal } = useValidators();
-  const { passwordValid } = useValidators();
-  const router = useRouter()
-  
-  const form = ref({
-    email: '',
-    user_name: '',
-    phone: '',
-    password: '',
-    confirmPassword: ''
-  })
-  
-  const Submit = () => {
-    registration(form.value)
-    router.push("/login")
-  }
-  
-  </script>
+  <q-page class="q-pa-md flex-center" style="width: 60%; margin: 0 auto">
+    <div>
+      <q-btn>
+        <router-link to="/">На Главную</router-link>
+      </q-btn>
+    </div>
+    <div>
+      <h5>Регистрация</h5>
+      <q-form @submit="Submit()">
+        <q-input
+          outlined
+          v-model="form.user_name"
+          label="ФИО"
+          type="text"
+          lazy-rules
+          :rules="[required]"
+        >
+        </q-input>
+        <q-input
+          outlined
+          v-model="form.password"
+          label="Введите ваш пароль"
+          type="text"
+          lazy-rules
+          :rules="[required, passwordValid]"
+        >
+        </q-input>
+        <q-input
+          outlined
+          v-model="form.confirmPassword"
+          label="Повторите ваш пароль"
+          type="text"
+          lazy-rules
+          :rules="[required, equal(form.password, form.confirmPassword)]"
+        >
+        </q-input>
+        <q-input
+          outlined
+          v-model="form.email"
+          label="Введите вашу почту"
+          type="email"
+          lazy-rules
+          :rules="[required]"
+        >
+        </q-input>
+        <q-input
+          outlined
+          v-model="form.phone"
+          label="Номер телефона"
+          type="tel"
+          mask="+7-###-### ## ##"
+          lazy-rules
+        >
+        </q-input>
+        <div class="q-mt-md">
+          <q-btn label="Отправить" type="submit" color="primary" />
+        </div>
+        <p class="q-pt-md">
+          Уже есть аккаунт? <router-link to="/Login">Войдите</router-link>
+        </p>
+      </q-form>
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import useValidators from "src/use/formValidation";
+import { registration } from "src/sdk/registration";
+import { useRouter } from "vue-router";
+
+const { required } = useValidators();
+const { equal } = useValidators();
+const { passwordValid } = useValidators();
+const router = useRouter();
+
+const form = ref({
+  email: "",
+  user_name: "",
+  phone: "",
+  password: "",
+  confirmPassword: "",
+});
+
+const Submit = () => {
+  registration(form.value);
+  router.push("/login");
+};
+</script>
